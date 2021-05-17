f5adaf58968254674ceae76bd8a7acb477a9a832
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-05-18 01:49:25 +0900

    기능: 카드 뒤집는 효과를 위한 css 작업 및 기능구현

---
+++ src/components/atoms/Cards/SurResultCardCenter.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import card from "../../../img/whywine_card.png";
+import cardSample from "../../../img/whywine_card_sample.png";
+import ReactCardFlip from "react-card-flip";
+
+const SurResultCardCenter = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isOpacity, setIsOpacity] = useState<boolean>(false);
+
+  const rotateCard = (e: any) => {
+    e.preventDefault();
+    setIsFlipped(!isFlipped);
+  };
+  useEffect(() => {
+    setIsOpacity(true);
+  }, []);
+
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div className="frontCard">
+        <img
+          style={{
+            opacity: isOpacity ? "1" : "0",
+          }}
+          className="cardCenter"
+          src={card}
+          alt="추천 와인 카드"
+          onClick={rotateCard}
+        />
+      </div>
+
+      <div className="backCard">
+        <img
+          className="wine"
+          src={cardSample}
+          alt="레드 와인 샘플"
+          onClick={rotateCard}
+        />
+      </div>
+    </ReactCardFlip>
+  );
+};
+
+export default SurResultCardCenter;
