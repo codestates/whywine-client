6ffbf3f8a2ab9af7d9c97253438e9dc78a1bb687
yoon-jisung <hmsj2380@gamil.com>
2021-05-17 20:07:39 +0900

    clear

--- src/components/templates/Landing.tsx
+++ src/components/templates/Landing.tsx
@@ -1,15 +1,15 @@
-
 import * as React from "react";
 import logo from "../../img/로고.png";
 import illustration from "../../img/랜딩페이지 일러스트.png";
+import Survey from "../organisms/surveyModal";
 
 const Landing: React.FC = () => {
   return (
     <div className="langdingImg">
       <img src={logo}></img>
       <img src={illustration}></img>
+      <Survey />
     </div>
   );
-
-
+};
 export default Landing;
