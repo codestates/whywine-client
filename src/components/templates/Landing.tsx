cd7b717ad5b06bf9936a873ef228c611af8b2c00
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-05-18 01:48:13 +0900

    수정: 랜딩 페이지에 survey modal이 사라져 추가해주었음

--- src/components/templates/Landing.tsx
+++ src/components/templates/Landing.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import logo from "../../img/로고.png";
 import illustration from "../../img/랜딩페이지 일러스트.png";
+import SurveyModal from "../organisms/surveyModal";
 
 const Landing: React.FC = () => {
   return (
     <div className="langdingImg">
       <img src={logo}></img>
       <img src={illustration}></img>
+      <SurveyModal />
     </div>
   );
 };
