1fa65e5a181a2b11e43e4fa641156766abad1c8c
yoon-jisung <hmsj2380@gamil.com>
2021-05-23 22:07:16 +0900

    기능:메인페이지에서 설문조사 넘어가능 모달 제작

--- src/components/organisms/Modal/surveyModal.tsx
+++ src/components/organisms/Modal/surveyModal.tsx
@@ -11,10 +11,13 @@ function SurveyModal({ isOpen }: Props) {
     <div className={isOpen ? "openModal modal" : "modal"}>
       {isOpen ? (
         <div className="SurveyModal">
-          <h1>와인 추천 받아보시겠습니까?</h1>
+          <h1>나의 와인 성향 테스트</h1>
+          <br></br>
+          <h1>어떤 와인 고를지 고민이시군요 ! </h1>
+          <p>잘 오셨습니다. 이제부터 몆가지 간단한 설문 조사를 통해</p>
+          <p>나에게 꼭 맞는 와인을 추천해드릴게요 따라오세요~</p>
           <div className="SurveyModalBtn">
             <GoToSurveyBtn />
-            <GoToMainBtn />
           </div>
         </div>
       ) : null}
--- src/components/templates/Survey.tsx
+++ src/components/templates/Survey.tsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
-import First from "./SurveyPages/SurveyFirstQ";
-import Second from "./SurveyPages/SurveySecondQ";
-import Third from "./SurveyPages/SurveyThirdQ";
+import Survey1 from "./SurveyPages/Survey1";
+import Survey2 from "./SurveyPages/Survey2";
+import Survey3 from "./SurveyPages/Survey3";
+import Survey4 from "./SurveyPages/Survey4";
+
 import Result from "./SurveyPages/SurveyResultQ";
+import UseScrollFadeIn from "../atoms/Scroll/UseScrollFadeIn";
 
 function Survey() {
+  const animatedItem = UseScrollFadeIn("Down", 1, 0.5);
   return (
     <div className="container">
-      <section>
-        <First />
+      <section className="survey">
+        <Survey1 animatedItem={animatedItem} />
       </section>
-      <section>
-        <Second />
+      <section className="survey">
+        <Survey2 />
       </section>
-      <section>
-        <Third />
+      <section className="survey">
+        <Survey3 />
       </section>
-      <section>
+      <section className="survey">
+        <Survey4 />
+      </section>
+      <section className="survey">
         <Result />
       </section>
     </div>
