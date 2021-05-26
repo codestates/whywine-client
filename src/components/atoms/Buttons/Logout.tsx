d240b27a2bc538887992d768e101aacebb783bab
jeasong <cjs5241@gmail.com>
2021-05-27 03:07:44 +0900

    디버깅:소셜로그인 수정

--- src/components/atoms/Buttons/Logout.tsx
+++ src/components/atoms/Buttons/Logout.tsx
@@ -8,8 +8,18 @@ interface Props {
 function Logout({ setIslogin }: Props) {
   const history = useHistory();
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     localStorage.removeItem("token");
+    await axios
+        .get(`https://localhost:4000/auth/logout`)
+        .then((res) => {
+          console.log(res.data.data);
+        })
+        .catch((err) => {
+          if (err) {
+            console.error();
+          }
+        });
     history.push("/");
     setIslogin(false);
   };
