fb023d503960a905d9d5f05a9b37d5c91e7ed160
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-06-02 16:18:17 +0900

    찜 목록 빈배열일시 사진 추

--- src/components/organisms/Categories/LikeListCategory.tsx
+++ src/components/organisms/Categories/LikeListCategory.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LikeCard from "../Cards/LikeCard";
-
+import LikeEmpty from "../../../img/like_empty3.png";
 interface State {
   userLikeWines: object[];
 }
@@ -8,14 +8,25 @@ interface State {
 const LikeListCategory = ({ userLikeWines }: State) => {
   console.log(userLikeWines);
   return (
-    <div className="mainSearchCategory">
-      <h2 className="mainCategoryName">내 찜 목록</h2>
-      <ul className="mainWineSearchBox">
-        {userLikeWines.map((wine, idx) => {
-          console.log(wine);
-          return <LikeCard userLikeWines={wine} key={idx} />;
-        })}
-      </ul>
+    <div>
+      {userLikeWines.length !== 0 ? (
+        <div className="mainSearchCategory">
+          <h2 className="mainCategoryName">내 찜 목록</h2>
+          <ul className="mainWineSearchBox">
+            {userLikeWines.map((wine, idx) => {
+              console.log(wine);
+              return <LikeCard userLikeWines={wine} key={idx} />;
+            })}
+          </ul>
+        </div>
+      ) : (
+        <div className="mainSearchCategory">
+          <h2 className="mainCategoryName">내 찜 목록</h2>
+          <img src={LikeEmpty} style={{ height: "400px", width: "400px" }} />
+          <h2>찜한 와인이 없습니다. </h2>
+          <h2>찜하기 버튼을 눌러 나만의 찜 목록을 만들어보세요.</h2>
+        </div>
+      )}
     </div>
   );
 };
