1ed4e3c1c3986ff5a053c5bd6b880b0828322c48
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-06-04 08:21:34 +0900

    css: 랜딩페이지 설문 모달 수정

--- src/components/organisms/Modal/surveyModal.tsx
+++ src/components/organisms/Modal/surveyModal.tsx
@@ -1,27 +1,33 @@
 import * as React from "react";
 import GoToMainBtn from "../../atoms/Buttons/GoToMainBtn";
 import GoToSurveyBtn from "../../atoms/Buttons/GoToSurveyBtn";
+import UseScrollFadeIn from "../../atoms/Scroll/UseScrollFadeIn";
 
 interface Props {
   isOpen: Boolean;
 }
 
 function SurveyModal({ isOpen }: Props) {
+  const animatedItem = UseScrollFadeIn("up", 1, 1);
   return (
-    <div className={isOpen ? "openModal modal" : "modal"}>
+    // <div className={isOpen ? "openModal modal" : "modal"}>
+    <div>
       {isOpen ? (
         <div className="SurveyModal">
           <h1>나의 와인 성향 테스트</h1>
           <br></br>
-          <h1>어떤 와인 고를지 고민이시군요 ! </h1>
-          <p>잘 오셨습니다. 이제부터 몆가지 간단한 설문 조사를 통해</p>
-          <p>나에게 꼭 맞는 와인을 추천해드릴게요 따라오세요~</p>
+          <h1>어떤 와인을 고를지 고민이시군요 ! </h1>
+          <p> 이제부터 몆가지 간단한 설문 조사를 통해</p>
+          <p>나에게 꼭 맞는 와인을 추천해드릴게요. </p>
+          <p>따라오세요~</p>
           <div className="SurveyModalBtn">
             <GoToSurveyBtn />
           </div>
         </div>
       ) : null}
     </div>
+
+    // </div>
   );
 }
 
