285cdb5d391bd5de8e3b06391e3778050c68ec5c
JaeHeon_Kim <jaeheon998795@gmail.com>
2021-06-06 02:32:59 +0900

    별점에 따른 별이 하나씩 추가되어 보이는 현상 수정

--- src/components/atoms/Icons/like.tsx
+++ src/components/atoms/Icons/like.tsx
@@ -7,14 +7,11 @@ interface Props {
   id: number;
 }
 
-
-
 // const active = useMemo(() => Active(boolean), [isLike]);
 
 function Like({ id }: Props) {
   const [isLike, setIsLike] = useState<boolean>();
   const [noLike, setNoLike] = useState(false);
-
 
   const getUserInfo = async () => {
     try {
@@ -26,20 +23,16 @@ function Like({ id }: Props) {
         JSON.stringify(data.data.data.userInfo)
       );
       // * 유저 정보 세션 스토리지 저장
-
     } catch (error) {
       console.dir(error);
     }
   };
 
-
   const handleLikeBtn = useCallback(async () => {
     setIsLike(!isLike);
-
     if (!isLike) {
       console.log(1);
       await axios
-
         .post(
           `${server}user/like`,
           { wineId: id },
@@ -49,13 +42,11 @@ function Like({ id }: Props) {
           }
         )
         .then(() => {
-
           getUserInfo();
         });
     } else {
       console.log(2);
       await axios
-
         .post(
           `${server}user/unlike`,
           { wineId: id },
@@ -65,49 +56,38 @@ function Like({ id }: Props) {
           }
         )
         .then(() => {
-
           // getUserInfo();
         })
         .catch((err) => console.dir(err));
       console.log("isLike 싫어요 요청", isLike);
     }
-  };
-
+  }, []);
 
   useEffect(() => {
-
     getUserInfo();
     if (sessionStorage.getItem("userInfo")) {
       let userInfo: any = sessionStorage.getItem("userInfo");
       userInfo = JSON.parse(userInfo);
       let { wines } = userInfo;
-
       // * 유저 정보에서 찜한 와인 목록 구조분해할당
       if (wines) {
-
         wines.map((el: any) => {
           if (id === el.id) {
             return setNoLike(true);
           } else {
-
-     
             return setIsLike(false);
           }
         });
       }
-
       // * 유저가 찜한 와인 배열에서 같은 와인 id가 있으면 islike 상태를 true로 반환한다.
     }
   }, []);
-
   return (
-
     <i
       onClick={() => handleLikeBtn()}
       className={isLike ? "like icon-red" : "unlike icon-black"}
     ></i>
   );
 }
-
 
 export default Like;
