27c179952cf87deabfef0b708b10385d7ed441a8
yoon-jisung <hmsj2380@gmail.com>
2021-06-07 00:58:04 +0900

    기능: 랜딩페이지 완성

--- src/components/templates/LandingPage/FourthPage.tsx
+++ src/components/templates/LandingPage/FourthPage.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
-import { useHistory } from "react-router-dom";
-import GoToTop from "../../atoms/Buttons/GoToTop";
+import MainCommentGif from "../../../gif/MainComment.gif";
+import UseScrollFadeIn from "../../atoms/Scroll/UseScrollFadeIn";
 
 function FourthPage() {
-  const history = useHistory();
+  const animatedItem1 = UseScrollFadeIn("up", 1, 0.5);
+  const animatedItem2 = UseScrollFadeIn("down", 2, 1);
   return (
     <div>
-      <div className="FourthPage_Summery">
-        <h1>와인이 처음이어도 실패하지 않게</h1>
-        <i
-          className="fas fa-arrow-alt-circle-right"
-          onClick={() => history.push("/survey")}
-        ></i>
+      <div className="FourthPage_Summery" {...animatedItem1}>
+        <h1>다른 사람들은 어떻게 생각하는지</h1>
+        <h1>내 생각을 자유롭게 공유하고</h1>
+        <h1>마음에 드셨으면 찜하세요 !</h1>
       </div>
+      <img
+        src={MainCommentGif}
+        className="FourthPageGif"
+        {...animatedItem2}
+      ></img>
     </div>
   );
 }
--- src/components/templates/LandingPage/SecondPage.tsx
+++ src/components/templates/LandingPage/SecondPage.tsx
@@ -1,19 +1,41 @@
 import * as React from "react";
 import UseScrollFadeIn from "../../atoms/Scroll/UseScrollFadeIn";
+import useScrollCount from "../../atoms/Scroll/UseScrollCount";
+import SurveyGif from "../../../gif/Survey.gif";
 
 function SecondPage() {
-  const animatedItem = UseScrollFadeIn("up", 2, 1);
-
-  React.useEffect(() => {}, []);
+  const animatedItem1 = UseScrollFadeIn("up", 1, 0.5);
+  const animatedItem2 = UseScrollFadeIn("down", 2, 1);
 
   return (
     <div>
-      <div className="SecondPage_Summery" {...animatedItem}>
-        <h1>특별한날에 와인이 필요하시나요 ?</h1>
-        <h1>어떤 와인을 골라야할지 고민이신가요 ? </h1>
+      <div className="SecondPage_Summery" {...animatedItem1}>
+        <h1>설문을 하고</h1>
+        <h1>준비된 알고리즘으로</h1>
+        <h1>내 취향 와인을 확인하세요.</h1>
       </div>
+
+      <img className="secondGif" src={SurveyGif} {...animatedItem2}></img>
     </div>
   );
 }
 
 export default SecondPage;
+
+//사용자의 취향에 따른 최고의 와인 TOP3 추천 뿐만 아니라,
+
+{
+  /* <div {...animatedItem1} className="SecondPage_Summery1">
+        <h1>사용자의 취향에 따른 최고의 와인 TOP3 추천 뿐만 아니라,</h1>
+        <h1>관심있는 태그와 와인을 찜하고,</h1>
+        <h1>좋아하는 와인에 리뷰를 남겨</h1>
+        <h1>다른 사용자들과 이야기를 나눌 수 있어요!</h1>
+      </div>
+      <div {...animatedItem2}>
+        <h1>whywine에서 전세계</h1>
+        <h1>
+          {" 총"}
+          <span {...animatedItem3}></span>개의 달하는 와인들을 만나보세요
+        </h1>
+      </div> */
+}
