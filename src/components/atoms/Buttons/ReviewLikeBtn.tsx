5b8319dd05787828db12f41bdcf6003a86bb9f14
Youjin Lee <youjinlee1997@gmail.com>
2021-06-07 17:08:54 +0900

    HOTFIX: comment/like axios 요청 수정

--- src/components/atoms/Buttons/ReviewLikeBtn.tsx
+++ src/components/atoms/Buttons/ReviewLikeBtn.tsx
@@ -1,16 +1,54 @@
 import React from "react";
+import axios from "axios";
+
+require("dotenv").config();
+
+const server = process.env.REACT_APP_API_SERVER;
 
 interface Props {
   like: boolean;
+  good_count: number;
+  handleComments: () => void;
+  commentId: number;
 }
 
-const ReviewLikeBtn = ({ like }: Props) => {
+const ReviewLikeBtn = ({
+  like,
+  good_count,
+  handleComments,
+  commentId,
+}: Props) => {
+  const handleLikeCount = async () => {
+    await axios
+      .post(
+        `${server}comment/good`,
+        { data: { commentId: commentId } },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((data) => {
+        console.dir(data);
+        handleComments();
+      })
+      // * 댓글 삭제 후 handleComments 함수 실행으로 commentsList 상태 변경해 재랜딩
+      .catch((err) => {
+        console.log(commentId);
+        console.dir(err);
+      });
+  };
+
   return (
-    <div>
+    <div className="like_Btn">
       {like ? (
-        <button className="reviewLikeBtn">좋아요</button>
+        <button onClick={() => handleLikeCount()} className="reviewLikeBtn">
+          <i className="fas fa-thumbs-up"></i> : {good_count}
+        </button>
       ) : (
-        <button className="reviewLikeBtn">싫어요</button>
+        <button className="reviewLikeBtn">
+          <i className="fas fa-thumbs-down"></i>
+        </button>
       )}
     </div>
   );
